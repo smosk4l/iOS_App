dd3a29fc26215f1e775c484956b6913c11c78d72
sm8sk4l <sebastianmoskal@protonmail.com>
2023-06-04 14:04:42 +0200

    basic logic done

--- App.js
+++ App.js
@@ -1,32 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import Card from "./components/Card";
 import { shuffleArray } from "./utils/arrayUtils";
 
 const cards = [
-  "🥹",
+  // "🥹",
   "🗣️",
   "🦷",
   "🍑",
   "🌪️",
   "🌎",
   "🐷",
-  "🪝",
-  "⚛️",
-  "🔑",
-  "🥕",
-  "🥑",
-  "👻",
-  "🥶",
-  "🥵",
+  // "🪝",
+  // "⚛️",
+  // "🔑",
+  // "🥕",
+  // "🥑",
+  // "👻",
+  // "🥶",
+  // "🥵",
 ];
 
 const App = () => {
-  const [board, setBoard] = useState(() => shuffleArray([...cards]));
+  const [board, setBoard] = useState(() => shuffleArray([...cards, ...cards]));
   const [selectedCards, setSelectedCards] = useState([]);
   const [matchedCards, setMatchedCards] = useState([]);
   const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    if (selectedCards.length < 2) return;
+    if (board[selectedCards[0]] === board[selectedCards[1]]) {
+      setMatchedCards([...matchedCards, ...selectedCards]);
+      setSelectedCards([]);
+    } else {
+      const timeoutId = setTimeout(() => setSelectedCards([]), 1000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [selectedCards]);
 
   const handleTapCard = (index) => {
     if (selectedCards.length >= 2 || selectedCards.includes(index)) return;
@@ -34,9 +45,19 @@ const App = () => {
     setScore(score + 1);
   };
 
+  const didPlayerWin = () => matchedCards.length === board.length;
+
+  const resetGame = () => {
+    setMatchedCards([]);
+    setScore(0);
+    setSelectedCards([]);
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Memory</Text>
+      <Text style={styles.title}>
+        {didPlayerWin() ? "Congratulations 🥂" : "Memory"}
+      </Text>
+      <Text style={styles.title}>Score: {score}</Text>
       <View style={styles.board}>
         {board.map((card, index) => {
           const isTurnedOver =
@@ -52,7 +73,7 @@ const App = () => {
           );
         })}
       </View>
-
+      {didPlayerWin() || <Button onPress={resetGame} title="Play again" />}
       <StatusBar style="light" />
     </View>
   );
